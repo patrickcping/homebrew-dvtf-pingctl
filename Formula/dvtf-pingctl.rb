48ced4edee7a05c982641a2852b4980fe37f92e7
Patrick Cowland <patrickcowland@pingidentity.com>
2024-08-27 16:05:52 +0100

    update tap

--- Formula/dvtf-pingctl.rb
+++ Formula/dvtf-pingctl.rb
@@ -8,22 +8,22 @@ class DvtfPingctl < Formula
   
     on_macos do
       on_arm do
-        url "https://github.com/patrickcping/dvtf-pingctl/releases/download/#{version}/dvtf-pingctl_darwin_arm64.tar.gz"
+        url "https://github.com/patrickcping/dvtf-pingctl/releases/download/v#{version}/dvtf-pingctl_darwin_arm64.tar.gz"
         sha256 "aa7d951d0de373aa6424b867965057805a6026dcdf8fd23f9ff53bf1fcae89f1"
       end
       on_intel do
-        url "https://github.com/patrickcping/dvtf-pingctl/releases/download/#{version}/dvtf-pingctl_darwin_amd64.tar.gz"
+        url "https://github.com/patrickcping/dvtf-pingctl/releases/download/v#{version}/dvtf-pingctl_darwin_amd64.tar.gz"
         sha256 "66067e414cf6551cd3df2f8eeb6291134ed92728417c68a83a61c13f2fa6f691"
       end
     end
   
     on_linux do
       on_arm do
-        url "https://github.com/patrickcping/dvtf-pingctl/releases/download/#{version}/dvtf-pingctl_linux_arm64.tar.gz"
+        url "https://github.com/patrickcping/dvtf-pingctl/releases/download/v#{version}/dvtf-pingctl_linux_arm64.tar.gz"
         sha256 "7a6c9bc1814a0c9580b6ab991c7ad12591149630e27cb9533eb697eba146a76b"
       end
       on_intel do
-        url "https://github.com/patrickcping/dvtf-pingctl/releases/download/#{version}/dvtf-pingctl_linux_amd64.tar.gz"
+        url "https://github.com/patrickcping/dvtf-pingctl/releases/download/v#{version}/dvtf-pingctl_linux_amd64.tar.gz"
         sha256 "5fdf2edce9e9cfb478420eec315abd345c58b576920758cfe43bd5773848a8b0"
       end
     end
